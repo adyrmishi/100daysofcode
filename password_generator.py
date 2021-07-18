580fa1d45290e09a4f5df096c1c5a5fe2d81c14a
Angela <82290574+adyrmishi@users.noreply.github.com>
2021-07-18 19:21:24 +0100

    Add files via upload
    Day 5 - I haven't worked out how to randomise the order of the characters yet...

---
+++ password_generator.py
@@ -0,0 +1,30 @@
+# To do:
+# 1. Work out how to randomize the order once the characters for the password have been selected.
+
+# Password Generator Project
+
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+           'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+           'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+elements = [letters, numbers, symbols]
+
+for element_type in elements:
+  element_type = random.shuffle(element_type)
+
+choices = [nr_letters, nr_numbers, nr_symbols]
+
+password = []
+
+for i in range(0, 3):
+    password.append("".join(elements[i][:int(choices[i])]))
+password = "".join(password)
+print(password)
