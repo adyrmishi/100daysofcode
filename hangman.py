91457c4a252d0887d3538913cc50ed111364827f
Angela <82290574+adyrmishi@users.noreply.github.com>
2021-07-21 18:15:20 +0100

    Update hangman.py
    Improved the functionality by telling the user when they re-guess a letter or reminding them of the letter they guessed if it was incorrect.

--- hangman.py
+++ hangman.py
@@ -57,12 +57,14 @@ stages = ['''
 
 lives = 6
 
+import hangman_words
+import hangman_art
 import random
-word_list = ["aardvark", "baboon", "camel"]
-chosen_word = random.choice(word_list)
 
-print("Welcome to Hangman.")
-print(stages[6])
+word_list = hangman_words.word_list
+chosen_word = random.choice(word_list)
+print(chosen_word)
+print(hangman_art.logo)
 
 display = ['_'] * len(chosen_word)
 
@@ -71,12 +73,15 @@ remaining_to_be_found = display.count('_')
 while remaining_to_be_found != 0 and lives != 0:
   guess = input("Guess a letter: ").lower()
   for i in range(len(chosen_word)):
-    if chosen_word[i] == guess:
-      display[i] = chosen_word[i]
+    if chosen_word[i] == guess and guess in display:
+      print("You've already guessed that letter.")
+    elif chosen_word[i] == guess and guess not in display:
+        display[i] = chosen_word[i]
   if guess not in chosen_word:
+    print(f"You guessed {guess}, that's not in the word.")
     lives -= 1
   print(display)
-  print(stages[lives])
+  print(hangman_art.stages[lives])
   print(f"You have {lives} lives remaining.")
 else:
   if remaining_to_be_found == 0:
