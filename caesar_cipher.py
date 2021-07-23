cb65abf1da001197fb1cd9dce180cd4e2f5f9fa1
Angela <82290574+adyrmishi@users.noreply.github.com>
2021-07-23 22:20:27 +0100

    Update caesar_cipher.py
    Figured out how to let the user play again if they want.

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,11 +1,5 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-play_again = "yes"
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
 def caesar(direction,text,shift):
   output_text = ""
   for letter in text:
@@ -18,8 +12,13 @@ def caesar(direction,text,shift):
         output_text += alphabet[(alphabet.index(letter) - shift) % len(alphabet)]
   print(output_text)
 
-# while play_again == "yes":
-#   play_again = input("Do you want to go again? Type 'yes' or 'no'.").lower()
-#   caesar(direction, text, shift)
-# else:
-#   print("Goodbye.")
+play_again = "yes"
+
+while play_again == "yes":
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  caesar(direction, text, shift)
+  play_again = input("Do you want to go again? Type 'yes' or 'no'.").lower()
+else:
+  print("Goodbye.")
