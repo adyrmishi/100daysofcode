e2880f309458ef07b211d55818853aca4b6f90a4
Angela <82290574+adyrmishi@users.noreply.github.com>
2021-08-15 21:39:35 +0100

    Create guess_the_number.py
    Can't get the game to end when attempts are finished / right answer is guessed yet

---
+++ guess_the_number.py
@@ -0,0 +1,27 @@
+import random 
+
+difficulty = input("Welcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100.\nChoose a difficulty. Type 'easy' or 'hard': ").lower()
+
+if difficulty == 'hard':
+  attempts = 5
+else:
+  attempts = 10
+
+number = random.randint(1,100)
+guess = 0
+
+while attempts > 0 or guess != number:
+  guess = int(input(f"You have {attempts} attempts left to guess the number.\nMake a guess: "))
+  if guess == number:
+    guess = number
+  else:
+    attempts -= 1
+    if guess < number:
+      print("Too low.")
+    else:
+      print("Too high.")
+else:
+  if attempts <= 0:
+    print(f"You've run out of attempts. The right answer was {number}.")
+  else:
+    print(f"You've guessed the right number which was {number}!")
