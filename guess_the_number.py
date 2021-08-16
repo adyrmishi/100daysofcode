d2e878eca536fba44c76a3787afc7f568dec9b68
Angela <82290574+adyrmishi@users.noreply.github.com>
2021-08-16 21:10:10 +0100

    Update guess_the_number.py
    Working game!

--- guess_the_number.py
+++ guess_the_number.py
@@ -11,17 +11,18 @@ number = random.randint(1,100)
 guess = 0
 
 while attempts > 0 or guess != number:
+  if attempts == 0:
+    print(f"You've run out of attempts. The right answer was {number}.")
+    break
+  
   guess = int(input(f"You have {attempts} attempts left to guess the number.\nMake a guess: "))
+
   if guess == number:
-    guess = number
+    print(f"You've guessed the right number which was {number}!")
+    break
   else:
     attempts -= 1
     if guess < number:
       print("Too low.")
-    else:
+    elif guess > number:
       print("Too high.")
-else:
-  if attempts <= 0:
-    print(f"You've run out of attempts. The right answer was {number}.")
-  else:
-    print(f"You've guessed the right number which was {number}!")
